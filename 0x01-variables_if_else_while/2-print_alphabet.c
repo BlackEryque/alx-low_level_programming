b60747543ecaab97605b76ba024e895a2d4b3137
BlackEryque <ericksonamuyunzu@outlook.com>
2022-12-09 21:52:24 -0500

    Task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the letters of the alphabet in lower case
+ *
+ * Return: 0
+ *
+*/
+
+int main(void)
+{	
+	int x = "A"
+	for (; x < "Z", x++)
+		putchar(tolower(x))
+}			
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,5 @@
 0-positive_or_negative.c: reurns a random number
 
 1-last_digit.c: prints the last digit of a number
+
+2-print_alphabet.c: prints alphabet letters in lower case
