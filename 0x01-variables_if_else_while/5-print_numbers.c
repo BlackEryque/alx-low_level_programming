bbc7060870a072bd53d80007cf74e5e7c423caa6
BlackEryque <ericksonamuyunzu@outlook.com>
2022-12-12 09:34:56 -0500

    Task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints single digits of base 10 starting from 0 
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		putchar (x);
+	x++;
+	}
+}
+
