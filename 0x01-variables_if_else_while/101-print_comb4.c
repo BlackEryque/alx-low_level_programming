0d1e4c193dae0085bd2a634b874ac18df5eeb402
BlackEryque <ericksonamuyunzu@outlook.com>
2022-12-13 04:09:12 -0500

    Task11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of three diffrent digits,
+ * in acsending order
+ *
+ * Return: 0
+ *
+ */
+
+in main(void)
+{
+	int x, y, z;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = x + 1; y < 9; y++)
+		{
+			for (z = y + 1; z < 10; y++)
+			{
+				putchar ((x % 10) + '0');
+				putchar ((y % 10) + '0');
+				putchar ((z % 10) + '0');
+
+				if (x == 7 && x == 8 && y == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
