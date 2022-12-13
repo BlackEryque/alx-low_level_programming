c522aa5e8334ea7ed92f6aa7d3db407468ab7b2b
BlackEryque <ericksonamuyunzu@outlook.com>
2022-12-13 04:14:09 -0500

    Task11 cortion23

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -22,7 +22,7 @@ int main(void)
 				putchar ((y % 10) + '0');
 				putchar ((z % 10) + '0');
 
-				if (x == 7 && x == 8 && y == 9)
+				if (x == 7 && y == 8 && z == 9)
 					continue;
 				putchar(',');
 				putchar(' ');
