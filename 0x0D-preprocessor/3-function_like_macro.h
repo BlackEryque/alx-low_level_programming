b890b1af97c3233851a72611b31e0af728e61881
BlackEryque <117816801+BlackEryque@users.noreply.github.com>
2023-01-18 01:01:48 +0300

    Done this one

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,11 +1,6 @@
 #ifndef _3_FUNCTION_LIKE_MACRO_H
 #define _3_FUNCTION_LIKE_MACRO_H
 
-#define ABS(X) \
-  if (x < 0)
-    x = x * (-1);
-    return (x);
-  else
-    return (x);
-  endif
+#define ABS(X) ((x) < 0) ? -(x) : (x)
+
 #endif  
