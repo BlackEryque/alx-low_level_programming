4378c2fdbd461939b99d2ab316597196e087e811
blackeryque <ericksonamuyunzu@outlook.com>
2023-02-02 15:48:42 +0300

    updated version

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,12 +1,22 @@
-#include <stddef.h>
 #ifndef LISTS_H
 #define LISTS_H
+#include <stddef.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: pointer to the next node
+ *
+ * Descirption: singly linked node structure
+ *
+ */
+
 
 typedef struct listint_s
 {
-  int n;
-  struct listint_s *next;
-}listint_t;
+	int n;
+	struct listint_s *next;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
